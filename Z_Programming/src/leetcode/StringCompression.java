69924f7c6f2db8bd41646ca1f64f774faf8f0f15
Shyamsundar Lakshminarayan <lshyamsv@gmail.com>
2020-05-10 16:06:24 -0700

    Codingbat and Leetcode String Compression

---
+++ Z_Programming/src/leetcode/StringCompression.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class StringCompression {
+
+	public int compress(char[] chars) {
+		int index = 0;
+		int i = 0;
+
+		while (i < chars.length) {
+			int j = i;
+			while (j < chars.length && chars[j] == chars[i]) {
+				j++;
+			}
+
+			chars[index++] = chars[i];
+			if (j - i > 1) {
+				String count = j - i + "";
+				for (char c : count.toCharArray()) {
+					chars[index++] = c;
+				}
+			}
+			i = j;
+
+		}
+		return index;
+	}
+
+	public static void main(String[] args) {
+
+		StringCompression chr = new StringCompression();
+		char[] word = { 'a', 'a', 'b', 'b', 'c', 'c', 'c' };
+		System.out.println(chr.compress(word));
+
+	}
+
+}
