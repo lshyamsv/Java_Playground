cf74b72dfb11503f8c7c7c99594030aad2e6f00a
Shyamsundar Lakshminarayan <lshyamsv@gmail.com>
2020-05-31 18:28:41 -0700

    Leetcode Remove Element

---
+++ Z_Programming/src/leetcode/RemoveElement.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class RemoveElement {
+
+	public int removeElement(int[] nums, int val) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != val) {
+				nums[count] = nums[i];
+				count++;
+			}
+		}
+		return count;
+
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
