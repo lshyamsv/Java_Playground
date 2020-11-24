805d40fd94ac2697f8f1b66fb66ec2900e40792f
Shyamsundar Lakshminarayan <lshyamsv@gmail.com>
2020-11-23 16:57:10 -0800

    Vishwa Binary Search

--- Z_Programming/src/Vishwa_Search/Binarysearch.java
+++ Z_Programming/src/Vishwa_Search/Binarysearch.java
@@ -2,11 +2,43 @@ package Vishwa_Search;
 
 public class Binarysearch {
 
-    public static int  binarySearch(int [] arr, int element){
+    public static int binarySearch(int[] arr, int start, int end, int target) {
 
-        for (int i = 0;i<arr.length;i++) {
-            int mid = 0;
-        } return 0;
+        start = 0;
+        end = arr.length - 1;
+
+//        while (start <= end) {
+//            int mid = (start + end) / 2;
+//            if (target < mid) {
+//                start = 0;
+//                end = mid;
+//            } else if (target > mid) {
+//                start = mid+1;
+//            } else {
+//                System.out.println("Element found");
+//                return true;
+//            }
+//        }
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] < target) {
+                //            start = 0;
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+                //        end = arr[arr.length-1];
+            } else {
+                System.out.println("Element found");
+                return mid;
+            }
+        }
+        System.out.println("Element not found");
+        return -1;
     }
 
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 8, 10, 13, 19, 21, 23};
+        binarySearch(arr, 0, arr.length, 0);
+    }
 }
